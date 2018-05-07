3f698e894d3c0d5bf074d3cc8449e6cf631e36f5
mikhail.loginov <mloginov@codio.com>
2018-05-07 15:36:23 +0300

    fixed Block Chomping

--- codemirror/mode/yaml-stack/yaml-stack.js
+++ codemirror/mode/yaml-stack/yaml-stack.js
@@ -161,7 +161,7 @@ CodeMirror.defineMode("yaml-stack", function(config) {
       /* start of value of a pair */
       if (state.pairStart) {
         /* block literals */
-        if (stream.match(/^\s*(\||\>)\s*/)) { state.literal = true; return 'meta'; };
+        if (stream.match(/^\s*((\||\>)(\+|-)?)\s*/)) { state.literal = true; return 'meta'; }
         /* references */
         if (stream.match(/^\s*(\&|\*)[a-z0-9\._-]+\b/i)) { return 'variable-2'; }
         /* numbers */
